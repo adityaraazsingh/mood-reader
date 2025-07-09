24b4de83cc2b37a7860ff377dd5ceb8ad2213226
Aditya Raj Singh <adityarajsingh00721@gmail.com>
2025-07-09 12:28:06 +0530

    Routes Modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EmotionFormComponent } from './emotion-form/emotion-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EmotionFormComponent }, // default route
+  { path: '**', redirectTo: '' } // wildcard route to redirect unknown paths
+];
